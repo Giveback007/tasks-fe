767288904d3a27524124e96ca48ff4f144d7aedf
Dovy <dovy91@gmail.com>
2025-04-03 17:22:38 -0400

    build fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+function onBuildError(errType, err) {
+	console.log('\n\n\n\n// 🢃🢃🢃🢃🢃 // ----- // BUILD-ERROR // ----- // 🢃🢃🢃🢃🢃 //\n// 🢃🢃🢃🢃🢃 // ----- // BUILD-ERROR // ----- // 🢃🢃🢃🢃🢃 //\n\n');
+    console.log(`❌ [< ERROR >]: ${errType}:\n`, err, { ...err });
+	console.log('\n\n// 🢁🢁🢁🢁🢁 // ----- // BUILD-ERROR // ----- // 🢁🢁🢁🢁🢁 //\n// 🢁🢁🢁🢁🢁 // ----- // BUILD-ERROR // ----- // 🢁🢁🢁🢁🢁 //\n\n\n\n');
+
+	setTimeout(() => process.exit(1), 12_000);
+}
+
+process.on('unhandledRejection', (reason) => onBuildError('Unhandled Rejection', reason));
+process.on('uncaughtException', (error) => onBuildError('Uncaught Exception', error));
+
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
