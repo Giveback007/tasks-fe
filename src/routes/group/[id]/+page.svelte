5abbbb9ecf24c173f9018ec83818f383d63e54d4
Dovy <dovy91@gmail.com>
2025-04-10 12:11:53 -0400

    able to reset all tasks now for a list

--- src/routes/group/[id]/+page.svelte
+++ src/routes/group/[id]/+page.svelte
@@ -2,9 +2,9 @@
     import Checklist from "$lib/components/Checklist.comp.svelte";
     import ColorsSel from "$lib/components/ColorsSel.comp.svelte";
     import Tabs from "$lib/components/Tabs.comp.svelte";
-    import { getData, groups, listIsOpen, updItem, type _List } from "$lib/store/store";
+    import { getData, groups, listIsOpen, updItem, updMulti, type _List } from "$lib/store/store";
     import { useSortable } from "$lib/util/sortable.util.svelte";
-    import { uuid } from "$lib/util/utils.util";
+    import { debounceById, uuid } from "$lib/util/utils.util";
     import type { PageData } from "./$types";
 
     const {
@@ -63,6 +63,9 @@
             })
         }
     });
+
+    const elms: Dict<HTMLButtonElement> = {};
+    let resetTasks: null | str = $state(null)
 </script>
 
 {#if group}
@@ -115,7 +118,6 @@
                 <th>Tasks</th>
                 <th>%</th>
                 <th></th>
-                <th></th>
             </tr>
         </thead>
         <tbody bind:this={tableElm}>
@@ -141,32 +143,57 @@
                 <td class="w-24">{list.nDone} / {list.nTasks}</td>
                 <td class="w-6">{prc > -1 ? prc : 0}%</td>
                 <td class="w-6" style="padding: 0 0.25rem;">
-                    <button
-                        aria-label="edit-group"
-                        class="outline contrast"
-                        style="padding: 0.35rem 0.5rem;"
-                        onclick={() => {
-                            editModal = list;
-                            newName = list.name;
-                            setTimeout(() => inputElm?.select());
-                        }}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-[0.1em] size-[1.5em] inline-block">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                        </svg>
-                    </button>
-                </td>
-                <td class="w-6" style="padding: 0 0.25rem;">
-                    <button
-                        aria-label="edit-group"
-                        class="outline contrast"
-                        style="padding: 0.35rem 0.5rem;"
-                        onclick={() => delModal = list}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                        </svg>
-                    </button>
+                    <div role="group" style="margin: 0;">
+
+                        <button
+                            aria-label="edit-group"
+                            class="{list.id === resetTasks ? '' : 'outline contrast'}"
+                            style="padding: 0.35rem 0.5rem;"
+                            bind:this={elms[`${list.id}::reset-tasks`]}
+
+                            onclick={() => {
+                                if (resetTasks !== list.id) {
+                                    resetTasks = list.id
+                                    setTimeout(() => resetTasks === list.id && (resetTasks = null), 2500)
+                                } else {
+                                    // reset all the tasks
+                                    updMulti(list.tasks.map(t => ['upd', { ...t, done: false }]))
+                                    resetTasks = null
+                                }
+                            }}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </svg>
+                        </button>
+
+                        <button
+                            aria-label="edit-group"
+                            class="outline contrast"
+                            style="padding: 0.35rem 0.5rem;"
+                            onclick={() => {
+                                editModal = list;
+                                newName = list.name;
+                                setTimeout(() => inputElm?.select());
+                            }}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-[0.1em] size-[1.5em] inline-block">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                            </svg>
+                        </button>
+
+                        <button
+                            aria-label="edit-group"
+                            class="outline contrast"
+                            style="padding: 0.35rem 0.5rem;"
+                            onclick={() => delModal = list}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                        </button>
+
+                    </div>
                 </td>
             </tr>
         {/each}
